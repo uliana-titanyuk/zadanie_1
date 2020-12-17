ade3aba9d16c093a83f75e08cf1a79754fa08772
uliana-titanyuk <63346238+uliana-titanyuk@users.noreply.github.com>
2020-12-17 16:03:19 +0300

    Create Header.h

---
+++ Header.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include "Class.h"
+using namespace std;
+
+void prepr(ifstream* filename, List *lst, string name);                                  */
+void sprtr(string* str, int num, List *lst, string name);
+void mnplt_review(List *lst); 
+void start(); 
+void open_file(List *lst); 
+void vyvod_menu(); 
+void menu(List *lst); 
+void obrabotka(List *lst);
+void znach_po_im(List *lst); 
+void fanction();
+void autotest();
